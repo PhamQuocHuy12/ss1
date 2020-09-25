2bf697b388b28ef23a1711be2e611ca650d9fe66
PhamQuocHuy12 <1801040104@s.hanu.edu.vn>
2020-09-25 11:01:31 +0700

    update dict

--- AI/lab2/ex2.py
+++ AI/lab2/ex2.py
@@ -1,5 +1,6 @@
-n=int(input("Input a number "))
-d = dict()
-for x in range(1,n+1):
-    d[str(input("input character: "))]= str(input("input equivalent character: "))
-print(d)
+def AutoDict():
+    n=int(input("Input a number "))
+    d = dict()
+    for x in range(1,n+1):
+        d[str(input("input character: "))]= str(input("input equivalent character: "))
+    return d
